e1d234c3063b0add35a2ee04bde0251f4f40c820
thoanguyen2907 <thoanguyen290795@gmail.com>
2021-01-09 16:45:29 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter, Switch, Route, Link, useLocation } from 'react-router-dom'
+import { useTransition, animated } from 'react-spring'
+import './styles.css'
 
-ReactDOM.render(
-  <React.StrictMode>
+export default function App() {
+  const location = useLocation()
+  const transitions = useTransition(location, location => location.pathname, {
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  })
+  return transitions.map(({ item: location, props, key }) => (
+    <animated.div key={key} style={props}>
+      <Switch location={location}>
+        <Route path="/" exact component={A} />
+        <Route path="/a" component={A} />
+        <Route path="/b" component={B} />
+        <Route path="/c" component={C} />
+      </Switch>
+    </animated.div>
+  ))
+}
+
+const A = () => (
+  <div style={{ background: 'lightpink' }}>
+    <Link to="/b">A</Link>
+  </div>
+)
+
+const B = () => (
+  <div style={{ background: 'lightblue' }}>
+    <Link to="/c">B</Link>
+  </div>
+)
+
+const C = () => (
+  <div style={{ background: 'lightgreen' }}>
+    <Link to="/a">C</Link>
+  </div>
+)
+
+render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </BrowserRouter>,
+  document.getElementById('root'),
+)
